8ebba6d17533019b7c2c5423038aefe0618fa919
OluwaTobba <142856021+OluwaTobba@users.noreply.github.com>
2024-02-29 21:56:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "NGN",
+  signDisplay: "always",
+});
+
+const list = document.getElementById("transactionList");
+const form = document.getElementById("transactionForm");
+const status = document.getElementById("status");
+const balance = document.getElementById("balance");
+const income = document.getElementById("income");
+const expense = document.getElementById("expense");
+
+form.addEventListener("submit", addTransaction);
+
+function updateTotal() {
+  const incomeTotal = transactions
+    .filter((trx) => trx.type === "income")
+    .reduce((total, trx) => total + trx.amount, 0);
+
+  const expenseTotal = transactions
+    .filter((trx) => trx.type === "expense")
+    .reduce((total, trx) => total + trx.amount, 0);
+
+  const balanceTotal = incomeTotal - expenseTotal;
+
+  balance.textContent = formatter.format(balanceTotal).substring(1);
+  income.textContent = formatter.format(incomeTotal);
+  expense.textContent = formatter.format(expenseTotal * -1);
+}
+
+function renderList() {
+  list.innerHTML = "";
+
+  status.textContent = "";
+  if (transactions.length === 0) {
+    status.textContent = "No transactions.";
+    return;
+  }
+
+  transactions.forEach(({ id, name, amount, date, type }) => {
+    const sign = "income" === type ? 1 : -1;
+
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      <div class="name">
+        <h4>${name}</h4>
+        <p>${new Date(date).toLocaleDateString()}</p>
+      </div>
+
+      <div class="amount ${type}">
+        <span>${formatter.format(amount * sign)}</span>
+      </div>
+    
+      <div class="action">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" onclick="deleteTransaction(${id})">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75 14.25 12m0 0 2.25 2.25M14.25 12l2.25-2.25M14.25 12 12 14.25m-2.58 4.92-6.374-6.375a1.125 1.125 0 0 1 0-1.59L9.42 4.83c.21-.211.497-.33.795-.33H19.5a2.25 2.25 0 0 1 2.25 2.25v10.5a2.25 2.25 0 0 1-2.25 2.25h-9.284c-.298 0-.585-.119-.795-.33Z" />
+        </svg>
+      </div>
+    `;
+
+    list.appendChild(li);
+  });
+}
+
+renderList();
+updateTotal();
+
+function deleteTransaction(id) {
+  const index = transactions.findIndex((trx) => trx.id === id);
+  transactions.splice(index, 1);
+
+  updateTotal();
+  saveTransactions();
+  renderList();
+}
+
+function addTransaction(e) {
+  e.preventDefault();
+
+  const formData = new FormData(this);
+
+  transactions.push({
+    id: transactions.length + 1,
+    name: formData.get("name"),
+    amount: parseFloat(formData.get("amount")),
+    date: new Date(formData.get("date")),
+    type: "on" === formData.get("type") ? "income" : "expense",
+  });
+
+  this.reset();
+
+  updateTotal();
+  saveTransactions();
+  renderList();
+}
+
+function saveTransactions() {
+  transactions.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
